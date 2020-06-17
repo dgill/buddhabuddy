e88ceb07f4c4f5c4c5422501273bc898f51bc57f
Daniel Gill <daniel.gill.val@gmail.com>
2020-06-17 12:12:28 -0700

    Starting fresh with angular 9.0. Adding ng-gapi classes manually instead of using npm package as we wait for 9.1 fix that fixes the loading of service in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,36 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  GoogleApiModule, 
+  GoogleApiService, 
+  GoogleAuthService, 
+  NgGapiClientConfig, 
+  NG_GAPI_CONFIG,
+  GoogleApiConfig
+} from "./ng-gapi";
+
+let gapiClientConfig: NgGapiClientConfig = {
+  client_id: "CLIENT_ID",
+  discoveryDocs: ["https://analyticsreporting.googleapis.com/$discovery/rest?version=v4"],
+  scope: [
+      "https://www.googleapis.com/auth/analytics.readonly",
+      "https://www.googleapis.com/auth/analytics"
+  ].join(" ")
+};
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleApiModule.forRoot({
+      provide: NG_GAPI_CONFIG,
+      useValue: gapiClientConfig
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
